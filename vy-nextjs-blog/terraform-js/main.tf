af64136fb6bfb98dc78965726cd43a709c0cc005
Volkan <volkanyazc@gmail.com>
2025-02-02 15:55:24 +0300

    added viewer certificate, restrictions,forwarded rulesand default cache behavior

--- vy-nextjs-blog/terraform-js/main.tf
+++ vy-nextjs-blog/terraform-js/main.tf
@@ -63,7 +63,7 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
     comment = "OAI for Next.JS portfolio site"
 }
 
-# Cloudfron Distribution
+# Cloudfront Distribution
 resource "aws_cloudfront_distribution" "nextjs_distribution" {
     origin {
       domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
@@ -78,3 +78,36 @@ resource "aws_cloudfront_distribution" "nextjs_distribution" {
     is_ipv6_enabled = true
     comment = "Next.js portfolio site"
     default_root_object = "index.html"
+
+    default_cache_behavior {
+      allowed_methods = ["GET", "HEAD", "OPTIONS"]
+      cached_methods = ["GET", "HEAD"]
+      target_origin_id = "S3v-nextjs-portfolio-bucket"
+
+      forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+      }
+
+      viewer_protocol_policy = "redirect-to-https"
+      min_ttl = 0
+      default_ttl = 3600
+      max_ttl = 86400
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+    viewer_certificate {
+
+        cloudfront_default_certificate = true
+
+    }
+
+    
+}
+
